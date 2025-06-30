34a5d1a9555b71b7591bc6b53a2f9f457a3a86a1
Etieno Johnson <134455965+jhonsn007@users.noreply.github.com>
2025-06-30 20:47:55 +0100

    Delete Kickbite directory

--- Kickbite/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navLinks = document.getElementById("nav-links");
-  
-    hamburger.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
-  });
-  
